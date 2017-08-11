18aefb206ed9c6c8ac65fff79440f807aa8bfb5b
bxy <binbinbin915@live.cn>
2017-08-11 15:19:09 +0800

    chore(ci): fix travis

--- .travis.docker.sh
+++ .travis.docker.sh
@@ -1,6 +1,6 @@
 #!bash
 docker build -t builder -f Dockerfile.build . && docker run builder | docker build -t bxy09/es-gc:$TRAVIS_TAG -
 mkdir -p ~/.docker
-'echo "{\"auths\":{\"https://index.docker.io/v1/\": {\"auth\": \"$DOCKER_HUB_AUTH\"}}}" > ~/.docker/config.json'
+echo "{\"auths\":{\"https://index.docker.io/v1/\": {\"auth\": \"$DOCKER_HUB_AUTH\"}}}" > ~/.docker/config.json
 docker push bxy09/es-gc:$TRAVIS_TAG
 docker rmi -f builder
